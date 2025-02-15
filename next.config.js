3fbcd82b544753756285a180491308067d445fd0
seishu <oltoberry7@gmail.com>
2025-02-16 04:59:36 +0900

    0216back

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     //API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
-    API_ENDPOINT: process.env.API_ENDPOINT,
+    API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
   },
   output: 'standalone',
 };
