fe748197489f28cfa67f6bb4d08fc01cf94bc0bf
seishu <oltoberry7@gmail.com>
2025-02-16 01:54:15 +0900

    ENDPOINT4

--- src/app/customers/create/createCustomer.js
+++ src/app/customers/create/createCustomer.js
@@ -26,7 +26,7 @@ const createCustomer = async (formData) => {
       //return;
     //}
 
-  const res = await fetch(`http://localhost:8000/customers`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/customers`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: body_msg,
